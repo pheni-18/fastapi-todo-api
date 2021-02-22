89d4802af291cf6b77e8aca526f08fe39372c5fd
pheni-18 <pheni.engineer@gmail.com>
2021-02-22 22:51:07 +0900

    mod: crud

--- crud.py
+++ crud.py
@@ -3,31 +3,33 @@ from models import Todo
 from schemas import TodoCreate, TodoUpdate
 
 
-def get_todo(db: Session, id_: int):
-    return db.query(Todo).filter(Todo.id == id_).first()
+class TodoRepo:
+    @staticmethod
+    def get_todo(db: Session, id_: int):
+        return db.query(Todo).filter(Todo.id == id_).first()
 
+    @staticmethod
+    def get_todo_list(db: Session):
+        return db.query(Todo).all()
 
-def get_todo_list(db: Session):
-    return db.query(Todo).all()
+    @staticmethod
+    def create_todo(db: Session, todo: TodoCreate):
+        db_todo = Todo(title=todo.title, description=todo.description, done=todo.done)
+        db.add(db_todo)
+        db.commit()
+        return db_todo
 
+    @staticmethod
+    def update_todo(db: Session, id_: int, todo: TodoUpdate):
+        db_todo = db.query(Todo).filter(Todo.id == id_).first()
+        db_todo.title = todo.title
+        db_todo.description = todo.description
+        db_todo.done = todo.done
+        db.commit()
+        return db_todo
 
-def create_todo(db: Session, todo: TodoCreate):
-    db_todo = Todo(title=todo.title, description=todo.description, done=todo.done)
-    db.add(db_todo)
-    db.commit()
-    return db_todo
-
-
-def update_todo(db: Session, id_: int, todo: TodoUpdate):
-    db_todo = db.query(Todo).filter(Todo.id == id_).first()
-    db_todo.title = todo.title
-    db_todo.description = todo.description
-    db_todo.done = todo.done
-    db.commit()
-    return db_todo
-
-
-def delete_todo(db: Session, id_: int):
-    db_todo = db.query(Todo).filter(Todo.id == id_)
-    db_todo.delete()
-    db.commit()
+    @staticmethod
+    def delete_todo(db: Session, id_: int):
+        db_todo = db.query(Todo).filter(Todo.id == id_)
+        db_todo.delete()
+        db.commit()
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from crud import (
-    get_todo_list as get_todo_list_, create_todo as create_todo_,
-    get_todo as get_todo_, update_todo as update_todo_, delete_todo as delete_todo_
-)
+from crud import TodoRepo
 from models import Base
 from schemas import TodoCreate, TodoUpdate, Todo
 from database import SessionLocal, engine
@@ -24,17 +21,17 @@ def get_db():
 
 @app.get("/api/todo", response_model=List[Todo])
 async def get_todo_list(db: Session = Depends(get_db)):
-    return get_todo_list_(db)
+    return TodoRepo.get_todo_list(db)
 
 
 @app.post("/api/todo",  response_model=Todo)
 async def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
-    return create_todo_(db, todo)
+    return TodoRepo.create_todo(db, todo)
 
 
 @app.get("/api/todo/{id}", response_model=Todo)
 async def get_todo(id_: int, db: Session = Depends(get_db)):
-    db_todo = get_todo_(db, id_)
+    db_todo = TodoRepo.get_todo(db, id_)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
     return db_todo
@@ -42,16 +39,16 @@ async def get_todo(id_: int, db: Session = Depends(get_db)):
 
 @app.patch("/api/todo/{id}",  response_model=Todo)
 async def update_todo(id_: int, todo: TodoUpdate, db: Session = Depends(get_db)):
-    db_todo = get_todo_(db, id_)
+    db_todo = TodoRepo.get_todo(db, id_)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
-    db_todo = update_todo_(db, id_, todo)
+    db_todo = TodoRepo.update_todo(db, id_, todo)
     return db_todo
 
 
 @app.delete("/api/todo/{id}")
 async def delete_todo(id_: int, db: Session = Depends(get_db)):
-    db_todo = get_todo_(db, id_)
+    db_todo = TodoRepo.get_todo(db, id_)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
-    delete_todo_(db, id_)
+    TodoRepo.delete_todo(db, id_)
